218cb5b9ae0ab2547063c89981174a74ac44df46
Rodrigo Nicastro <rofr4171@colorado.edu>
2024-04-15 13:29:34 -0600

    cleaning repo

--- assignment-1-rodrigonicastro/build/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/CUBoulder/CSCI2270/assignment-1-rodrigonicastro/build/googletest-src
-# Build directory: /mnt/c/CUBoulder/CSCI2270/assignment-1-rodrigonicastro/build/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- assignment-1-rodrigonicastro/build/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/CUBoulder/CSCI2270/assignment-1-rodrigonicastro/build/googletest-src/googlemock
-# Build directory: /mnt/c/CUBoulder/CSCI2270/assignment-1-rodrigonicastro/build/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
